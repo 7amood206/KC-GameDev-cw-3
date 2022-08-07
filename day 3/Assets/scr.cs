eb0829ad017308c431c74d08c2622edc2e790a45
7amood206 <mohammadsulaiman206@gmail.com>
2022-08-07 15:42:20 +0300

    added the script

--- day 3/Assets/scr.cs
+++ day 3/Assets/scr.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class scr : MonoBehaviour
 {
+
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        string heroName = "hero";
+        int heroPower = 20;
+        string villianName = "villian";
+        int villianPower = 10;
+
+        if (heroPower > villianPower)
+        {
+            print("hero is stronger");
+        }
+        else if (heroPower == villianPower)
+        {
+            print("they are tied");
+        }
+        else 
+        {
+            print("villian is stronger");
+        }
+
+        setSpeed(2.5f);
+        compareSpeed(3.5f , 2.5f);
+        }
+
+    float playerSpeed = 3.5f;
+
+    void setSpeed(float speed){
+        print(playerSpeed);
+        playerSpeed = speed;
+        print(playerSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void compareSpeed(float oldSpeed, float newSpeed){
+        if (oldSpeed > newSpeed){
+            print(oldSpeed);
+        }
+        else if (oldSpeed == newSpeed){
+            print(oldSpeed + "," + newSpeed);
+        }
+        else {
+            print(newSpeed);
+        }
     }
+
 }
+
